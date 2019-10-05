cad29329f9db0c5127b9ebb45d2becf56fbd3539
BlackGun <417632129@qq.com>
2019-10-05 15:33:31 +0100

    v0.1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotateSpeed;
+    CharacterController _controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
+        _controller.Move(move * Time.deltaTime * movementSpeed);
+        //r.velocity = (transform.forward * movementY + transform.right * movementX )* movementSpeed * Time.deltaTime;
+
+        float mouseX = Input.GetAxis("Mouse X");
+        
+
+        //float mouseY = Input.GetAxis("Mouse Y");
+        //Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * mouseX * Time.deltaTime);
+        //r.MoveRotation(deltaRotation * r.rotation);
+        transform.Rotate(0, mouseX * rotateSpeed * Time.deltaTime, 0);
+        //r.AddForce(new Vector3(movementX, 0, movementY) * movementSpeed * Time.deltaTime);
+        //this.gameObject.transform.Translate(new Vector3(movementX , 0, movementY) * movementSpeed * Time.deltaTime);
+    }
+}
